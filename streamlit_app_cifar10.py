c7546d85985c16eaba812fbdba232f4b3dbdcf6b
emmanuelgendy <emmanuel.gendy@gmail.com>
2025-06-24 23:25:54 +0200

    Adjusted streamlit app for cifar10

--- streamlit_app_cifar10.py
+++ streamlit_app_cifar10.py
@@ -3,38 +3,42 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# CIFAR-10 class names
+st.title("CIFAR-10 Image Classifier 🎯")
+
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
-st.title("CIFAR-10 Image Classifier")
-
-# Load pre-trained model (using TensorFlow Keras applications)
 @st.cache_resource
 def load_model():
-    model = tf.keras.applications.MobileNetV2(weights=None, input_shape=(32, 32, 3), classes=10)
-    # For simplicity, we just simulate a trained model (in real case, load trained weights)
-    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    model = tf.keras.models.load_model("cifar10_model.h5")
     return model
 
 model = load_model()
 
-# Upload image
-uploaded_file = st.file_uploader("Choose a CIFAR-10 image (32x32)", type=["jpg", "jpeg", "png"])
+st.write("Choose an image source:")
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file).resize((32, 32))
-    st.image(image, caption='Uploaded Image', use_column_width=False)
+option = st.radio("Select input method:", ('Upload Image', 'Use Webcam'))
 
-    # Preprocess image
-    img_array = np.array(image) / 255.0
+image = None
+
+if option == 'Upload Image':
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file).convert("RGB")
+elif option == 'Use Webcam':
+    picture = st.camera_input("Take a picture")
+    if picture:
+        image = Image.open(picture).convert("RGB")
+
+if image:
+    image_resized = image.resize((32, 32))
+    st.image(image_resized, caption='Processed Image (resized to 32x32)', use_column_width=False)
+
+    img_array = np.array(image_resized) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    # Make prediction (dummy prediction here since model is untrained)
-    prediction = np.random.rand(10)  # Random predictions for demo purposes
-    predicted_class = class_names[np.argmax(prediction)]
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions)]
 
-    st.write(f"Predicted Class: **{predicted_class}**")
-
-    # Display probabilities
-    st.bar_chart(prediction)
+    st.write(f"### Predicted Class: **{predicted_class}**")
+    st.bar_chart(predictions[0])
